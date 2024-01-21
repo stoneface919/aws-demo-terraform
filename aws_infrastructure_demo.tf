62e784d5549d3f35eb465469a10d1f44d89b3829
stoneface <saulzenteno27@gmail.com>
2024-01-21 12:03:37 -0600

    network infrastructure setup

---
+++ aws_infrastructure_demo.tf
@@ -0,0 +1,145 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.33.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "cidr_blocks_aws" {
+    description = "CIDR block for demo insfrastructure"
+    type = list(object({
+        cidr_block = string,
+        name = string
+    }))
+}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+
+# CREATE VPC
+resource "aws_vpc" "app01_vpc" {
+    cidr_block = var.cidr_blocks_aws[0].cidr_block
+    tags = {
+      Name: "${var.env_prefix}-${var.cidr_blocks_aws[0].name}"
+    } 
+}
+
+# CREATE ONE SUBNET IN ONE SINGLE AZ
+resource "aws_subnet" "app01-subnet" {
+    vpc_id = aws_vpc.app01_vpc.id
+    tags = {
+      Name: "${var.env_prefix}-${var.cidr_blocks_aws[1].name}"
+    }
+    cidr_block = var.cidr_blocks_aws[1].cidr_block
+    availability_zone = "us-east-2a"
+}
+
+# CREATE A ROUTE TABLE INSIDE THE SUBNET AND LINK IT TO AN INTERNET GATEWAY
+# VIRTUAL ROUTER 
+resource "aws_route_table" "app01-route_table" {
+    vpc_id = aws_vpc.app01_vpc.id
+    route {
+        # IG
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.app01-internet_gateway.id
+
+    }
+    tags = {
+      Name: "${var.env_prefix}-rtb"
+    }
+
+}
+
+# CREATING IG
+# VIRTUAL WAN CONNECTION - MODEM
+resource "aws_internet_gateway" "app01-internet_gateway" {
+    vpc_id = aws_vpc.app01_vpc.id
+    tags = {
+      Name: "${var.env_prefix}-igw"
+    }
+}
+
+# ASSOCIATION OF RTB ANMD SUBNET
+resource "aws_route_table_association" "app01-rtb-subnet" {
+    subnet_id = aws_subnet.app01-subnet.id
+    route_table_id = aws_route_table.app01-route_table.id
+}
+
+# CREATE A SECURITY GROUP - CONFIGURE INBOUND/OUTBOUND RULES
+resource "aws_security_group" "app01-sg" {
+    name = "app01-sg"
+    description = "Allow SSH and NGNIX inbound traffic"
+    vpc_id = aws_vpc.app01_vpc.id
+    # ssh alloww
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+    # allow proxy traffic
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+
+    }
+    tags = {
+      Name: "${var.env_prefix}-app01-sg"
+    }
+}
+
+# FETCH AMI -> EC2 INSTANCE
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+      name = "name"
+      values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+
+}
+
+# CREATE EC2 INTANCE AND ASSOCIATE WITH SECURITY GROUP/VPC
+resource "aws_instance" "app01-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.app01-sg.id]
+    # security_groups = [aws_security_group.app01-sg.id]
+    subnet_id = aws_subnet.app01-subnet.id
+    availability_zone = var.avail_zone
+    associate_public_ip_address = true
+    key_name = "ec2-tutanota"
+    
+}
+
+
+
+
+
+output "amazon-linux-latest" {
+    value = data.aws_ami.latest-amazon-linux-image
+  
+}
+
+# DEPLOY EC2 INSTANCE WITH A DOCKER CONTAINER
+
--- main.tf
+++ main.tf
@@ -1,68 +1,70 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "5.33.0"
-    }
-  }
-}
-
-provider "aws" {
-    region = "us-east-2"
-}
-
-variable "cidr_blocks_staging" {
-  description = "CIDR blocks for the dev environment"
-  type = list(object(
-    {cidr_block = string,
-    name = string}
-  ))
-}
 
 
-resource "aws_vpc" "staging_vpc" {
-    cidr_block = var.cidr_blocks_staging[0].cidr_block
-    tags = {
-      Name: var.cidr_blocks_staging[0].name
-      vpc_env: var.cidr_blocks_staging[0].name
-    }
+# variable "cidr_blocks_staging" {
+#   description = "CIDR blocks for the dev environment"
+#   type = list(object(
+#     {cidr_block = string,
+#     name = string}
+#   ))
+# }
+
+
+# resource "aws_vpc" "staging_vpc" {
+#     cidr_block = var.cidr_blocks_staging[0].cidr_block
+#     tags = {
+#       Name: var.cidr_blocks_staging[0].name
+#       vpc_env: var.cidr_blocks_staging[0].name
+#     }
   
-}
+# }
 
 
-resource "aws_subnet" "staging_subnet01" {
-    vpc_id = aws_vpc.staging_vpc.id
-    cidr_block = var.cidr_blocks_staging[1].cidr_block
-    availability_zone = "us-east-2a"
-    tags = {
-      Name: var.cidr_blocks_staging[1].name
-}
-}
+# resource "aws_subnet" "staging_subnet01" {
+#     vpc_id = aws_vpc.staging_vpc.id
+#     cidr_block = var.cidr_blocks_staging[1].cidr_block
+#     availability_zone = "us-east-2a"
+#     tags = {
+#       Name: var.cidr_blocks_staging[1].name
+# }
+# }
 
-data "aws_vpc" "existing_vpc" {
-    default = true
-}
+# data "aws_vpc" "existing_vpc" {
+#     default = true
+# }
 
-resource "aws_subnet" "staging_subnet02" {
-    cidr_block = var.cidr_blocks_staging[2].cidr_block
-    vpc_id = aws_vpc.staging_vpc.id
-    availability_zone = "us-east-2a"
-    tags = {
-      Name: var.cidr_blocks_staging[2].name
-    }
+# resource "aws_subnet" "staging_subnet02" {
+#     cidr_block = var.cidr_blocks_staging[2].cidr_block
+#     vpc_id = aws_vpc.staging_vpc.id
+#     availability_zone = "us-east-2a"
+#     tags = {
+#       Name: var.cidr_blocks_staging[2].name
+#     }
   
-}
+# }
 
 
-output "staging_vpc_id" {
-    value = aws_vpc.staging_vpc.id
-}
+# output "staging_vpc_id" {
+#     value = aws_vpc.staging_vpc.id
+# }
 
-output "staging_subnet_id01" {
-    value = aws_subnet.staging_subnet01.id
-}
+# output "staging_subnet_id01" {
+#     value = aws_subnet.staging_subnet01.id
+# }
 
 
-output "staging_subnet_id02" {
-    value = aws_subnet.staging_subnet02.id
-}
+# output "staging_subnet_id02" {
+#     value = aws_subnet.staging_subnet02.id
+# }
+
+
+# # CREATE VPC
+
+# # CREATE ONE SUBNET IN ONE SINGLE AZ
+
+# # ALLOW TRAFFIC FROM EC32 THROUGHT AN INTERNET GATEWAY
+
+# # DEPLOY EC2 INSTANCE WITH A DOCKER CONTAINER
+
+# # OPEN FIREWALL RULES TO THE NGNIX SERVER
+
+# # CREATE A SECURITY GROUP TO ALLOW NGINX AND SSH TRAFFIC 
