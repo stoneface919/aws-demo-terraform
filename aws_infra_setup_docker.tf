0f188d59aba9480ceb3c9c1ab75040fb3f4ea772
sxtho <saulzenteno27@gmail.com>
2024-01-22 13:29:24 -0600

    refactoring for app02

--- aws_infra_setup_docker.tf
+++ aws_infra_setup_docker.tf
@@ -25,7 +25,7 @@ variable "instance_type" {}
 variable "key_pub" {}
 
 # CREATE VPC
-resource "aws_vpc" "app01_vpc" {
+resource "aws_vpc" "app02_vpc" {
     cidr_block = var.cidr_blocks_aws[0].cidr_block
     tags ={
       Name: "${var.env_prefix}-${var.cidr_blocks_aws[0].name}"
@@ -33,8 +33,8 @@ resource "aws_vpc" "app01_vpc" {
 }
 
 # CREATE ONE SUBNET IN ONE SINGLE AZ
-resource "aws_subnet" "app01-subnet" {
-    vpc_id = aws_vpc.app01_vpc.id
+resource "aws_subnet" "app02-subnet" {
+    vpc_id = aws_vpc.app02_vpc.id
     tags = {
       Name: "${var.env_prefix}-${var.cidr_blocks_aws[1].name}"
     }
@@ -44,12 +44,12 @@ resource "aws_subnet" "app01-subnet" {
 
 # CREATE A ROUTE TABLE INSIDE THE SUBNET AND LINK IT TO AN INTERNET GATEWAY
 # VIRTUAL ROUTER 
-resource "aws_route_table" "app01-route_table" {
-    vpc_id = aws_vpc.app01_vpc.id
+resource "aws_route_table" "app02-route_table" {
+    vpc_id = aws_vpc.app02_vpc.id
     route {
         # IG
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.app01-internet_gateway.id
+        gateway_id = aws_internet_gateway.app02-internet_gateway.id
 
     }
     tags = {
@@ -60,24 +60,24 @@ resource "aws_route_table" "app01-route_table" {
 
 # CREATING IG
 # VIRTUAL WAN CONNECTION - MODEM
-resource "aws_internet_gateway" "app01-internet_gateway" {
-    vpc_id = aws_vpc.app01_vpc.id
+resource "aws_internet_gateway" "app02-internet_gateway" {
+    vpc_id = aws_vpc.app02_vpc.id
     tags = {
       Name: "${var.env_prefix}-igw"
     }
 }
 
 # ASSOCIATION OF RTB ANMD SUBNET
-resource "aws_route_table_association" "app01-rtb-subnet" {
-    subnet_id = aws_subnet.app01-subnet.id
-    route_table_id = aws_route_table.app01-route_table.id
+resource "aws_route_table_association" "app02-rtb-subnet" {
+    subnet_id = aws_subnet.app02-subnet.id
+    route_table_id = aws_route_table.app02-route_table.id
 }
 
 # CREATE A SECURITY GROUP - CONFIGURE INBOUND/OUTBOUND RULES
-resource "aws_security_group" "app01-sg" {
-    name = "app01-sg"
+resource "aws_security_group" "app02-sg" {
+    name = "app02-sg"
     description = "Allow SSH and NGNIX inbound traffic"
-    vpc_id = aws_vpc.app01_vpc.id
+    vpc_id = aws_vpc.app02_vpc.id
     # ssh alloww
     ingress {
         from_port = 22
@@ -121,15 +121,15 @@ data "aws_ami" "latest-amazon-linux-image" {
 }
 
 # CREATE EC2 INTANCE AND ASSOCIATE WITH SECURITY GROUP/VPC
-resource "aws_instance" "app01-server" {
+resource "aws_instance" "app02-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.app01-sg.id]
+    vpc_security_group_ids = [aws_security_group.app02-sg.id]
     # security_groups = [aws_security_group.app01-sg.id]
-    subnet_id = aws_subnet.app01-subnet.id
+    subnet_id = aws_subnet.app02-subnet.id
     availability_zone = var.avail_zone
     associate_public_ip_address = true
-    key_name = aws_key_pair.app01-keypair.key_name
+    key_name = aws_key_pair.app02-keypair.key_name
     user_data_base64 = "IyFiaW4vYmFzaAp5dW0gdXBkYXRlIC15ICYmIHl1bSBpbnN0YWxsIC15IGRvY2tlcgpzeXN0ZW1jdGwgc3RhcnQgZG9ja2VyCnVzZXJtb2QgLWFHIGRvY2tlciBlYzItdXNlcgpkb2NrZXIgcnVuIC1wIDgwODA6ODAgbmdpbng="
     metadata_options {
      
@@ -138,8 +138,8 @@ resource "aws_instance" "app01-server" {
 
 
 # CREATE KEY PAIR
-resource "aws_key_pair" "app01-keypair" {
-  key_name = "app01-server-key"
+resource "aws_key_pair" "app02-keypair" {
+  key_name = "app02-server-key"
   public_key = var.key_pub
 }
 
--- databases.tf
+++ databases.tf
@@ -1,26 +1,26 @@
-resource "aws_db_instance" "app01-rds_database" {
-    allocated_storage = 10
-    instance_class = "db.t3.micro"
-    username = "stoneface_admin"
-    password = "Stoneface&&33&&1998"
-    skip_final_snapshot = true
-    engine = "mysql"
-    engine_version = "5.7"
-    availability_zone = "us-east-2a"
-    identifier = "prod-dbinstance-mysql"
-    tags = {
-        Name: "prod-dbinstance-mysql"
-    }
-    backup_retention_period = 7
-    publicly_accessible = true
+# resource "aws_db_instance" "app01-rds_database" {
+#     allocated_storage = 10
+#     instance_class = "db.t3.micro"
+#     username = "stoneface_admin"
+#     password = "Stoneface&&33&&1998"
+#     skip_final_snapshot = true
+#     engine = "mysql"
+#     engine_version = "5.7"
+#     availability_zone = "us-east-2a"
+#     identifier = "prod-dbinstance-mysql"
+#     tags = {
+#         Name: "prod-dbinstance-mysql"
+#     }
+#     backup_retention_period = 7
+#     publicly_accessible = true
     
 
-}
+# }
 
 
 
 
 
-output "rds-dbinstance-mysql" {
-    value = aws_db_instance.app01-rds_database.id  
-}
+# output "rds-dbinstance-mysql" {
+#     value = aws_db_instance.app01-rds_database.id  
+# }
